c83acf59dd759de7b5e071d1b859368012500315
zakykurniawan13 <zakikurniawan395@gmail.com>
2022-05-24 23:19:36 +0700

    integrasi API Home

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -3,6 +3,7 @@ import "./Detail.scss";
 import React, { useState } from "react";
 
 import { Layout } from "../../layout/Layout";
+import chris from "../../assets/images/chris.jpg";
 import extdetail from "../../assets/images/extractiondetail.jpg";
 
 export const Detail = () => {
@@ -41,16 +42,98 @@ export const Detail = () => {
           <div className="main-detail-title1"> Sin City</div>
           <div className="main-detail-rating">
             {handleRating()}
-            <div className="main-detail-rating-title">4.3/5</div>
+            <div
+              className="main-detail-rating-title"
+              style={{
+                color: "white",
+              }}
+            >
+              4.3
+            </div>
+            <div
+              className="main-detail-rating-title"
+              style={{
+                marginLeft: "0px",
+              }}
+            >
+              /5
+            </div>
             <div className="main-detail-rating-title">Thriller</div>
             <div className="main-detail-rating-title"> 2h 4min</div>
             <div className="main-detail-rating-title"> 2005</div>
-            <div className="main-detail-rating-title" style={{
-              border: "solid 1px #FFFFFF",
-              padding: "1px 5px"
-            }}>18+</div>
+            <div
+              className="main-detail-rating-title"
+              style={{
+                border: "solid 1px rgba(135, 135, 135, 0.68)",
+                padding: "1px 5px",
+              }}
+            >
+              18+
+            </div>
           </div>
-          <div className="main-detail-title2">deskripsi filmm</div>
+          <div className="main-detail-title2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            neque voluptatibus, optio excepturi placeat quas, dolor a omnis
+            nobis veniam cupiditate pariatur sint ratione nesciunt maxime minus
+            iure amet? Fugiat.
+          </div>
+          <div className="main-detail-title3">
+            <div className="main-detail-title3-1">PLAY MOVIE</div>
+            <div className="main-detail-title3-2">WATCH LATER</div>
+          </div>
+          <div className="credit">
+            <div className="credit-cast">
+              <div className="main-detail-title4-1">CAST</div>
+              <div className="cast-list">
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+                <div className="cast">
+                  <img className="cast-img" src={chris} />
+                  <div className="cast-name">Chriss hamsowrd</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="credit-director">
+              <div className="main-detail-title4-1">DIRECTED BY</div>
+              <div className="directed">Jon Belion</div>
+              <div className="directed">Adam Levine</div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import "./Home.scss";
 
+import React, { useEffect, useState } from "react";
+
 import { Layout } from "../../layout/Layout";
-import React from "react";
 import armydeadmovie from "../../assets/images/armydead.jpg";
+import axios from "axios";
 import brightmovie from "../../assets/images/bright.jpg";
 import extraction2movie from "../../assets/images/extraction2.jpg";
 import extractionmovie from "../../assets/images/extraction.jpg";
@@ -11,13 +13,59 @@ import rednoticemovie from "../../assets/images/red notice.jpg";
 import spidermanmovie from "../../assets/images/spiderman.jpg";
 
 export const Home = () => {
+  const [moviesTrending, setMoviesTrending] = useState([]);
+  const [recent, setRecent] = useState([]);
+
+  const getMovieTrending = async () => {
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/trending/movie/week?api_key=f4cf4882bb4a1025d54e7abd91962ecc",
+        { headers }
+      );
+      if (response.status === 200) {
+        setMoviesTrending(response.data.results);
+      }
+    } catch (error) {
+      console.log(error, "error");
+    }
+  };
+
+  const getRecent = async () => {
+    try {
+      const headers ={
+        "Content-Type": "appplication.json",
+      };
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=f4cf4882bb4a1025d54e7abd91962ecc&language=en-US&page=1",
+        { headers }
+      );
+      if (response.status === 200) {
+        setRecent(response.data.results);
+      }
+    } catch (error) {
+      console.log(error, "error");
+    }
+  };
+
+  
+  useEffect(() => {
+    getMovieTrending();
+    getRecent();
+  }, []);
+
   return (
     <Layout>
-      <div className="main-container"> 
+      <div className="main-container">
         <div className="main-left">
           <div className="main-home">
             <img className="main-home-foto" src={foto1} />
-            <div className="main-home-foto-title"> Watch Your Favourites Movie</div>
+            <div className="main-home-foto-title">
+              {" "}
+              Watch Your Favourites Movie
+            </div>
           </div>
           <div className="main-home-trending">
             <div className="main-home-trending-header">
@@ -25,171 +73,116 @@ export const Home = () => {
               <div className="main-home-trending-subtitle">See all</div>
             </div>
             <div className="movie-list-trending">
-              <div className="card-movie">
-                <img className="card-movie-foto" src={brightmovie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Bright</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2021</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
-                  </div>
-                </div>
-              </div>
-              
-              <div className="card-movie">
-                <img className="card-movie-foto" src={spidermanmovie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Spiderman Far From Home</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2021</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
+              {moviesTrending.map((item) => (
+                <div className="card-movie">
+                  <img
+                    className="card-movie-foto"
+                    src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
+                  />
+                  <div className="card-content">
+                    <div className="card-content-left">
+                      <div className="card-content-left-title">
+                        {item.title}
+                      </div>
+                      <div
+                        className="card-content-left-subtitle"
+                        style={{
+                          color: "#878787",
+                          fontSize: "12px",
+                        }}
+                      >
+                        {item.release_date}
+                      </div>
+                    </div>
+                    <div className="card-content-right">
+                      <div
+                        className="card-content-action"
+                        style={{
+                          backgroundColor: "rgba(158,7,33,0.68)",
+                          borderRadius: "20px",
+                        }}
+                      >
+                        Watch Now
+                      </div>
+                      <div
+                        className="card-content-action"
+                        style={{
+                          backgroundColor: "#F6C600",
+                          color: "black",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        ImDb {item.vote_average}
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="card-movie">
-                <img className="card-movie-foto" src={extractionmovie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Extraction</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2015</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="card-movie">
-                <img className="card-movie-foto" src={extraction2movie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Extraction</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2020</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-
         </div>
         <div className="main-right">
           <div className="main-genre">
-          <div className="content-label">Genre</div>
-          <div className="list-genre">
-            <div className="genre-item" style={{
-              backgroundColor: "rgba(158,7,33,0.68)",
-            }
-
-            }>Action
+            <div className="content-label">Genre</div>
+            <div className="list-genre">
+              <div
+                className="genre-item"
+                style={{
+                  backgroundColor: "rgba(158,7,33,0.68)",
+                }}
+              >
+                Action
+              </div>
+              <div className="genre-item">Adventure</div>
+              <div className="genre-item">Comedy</div>
+              <div className="genre-item">Drama</div>
+              <div className="genre-item">Horor</div>
+              <div className="genre-item">Romance</div>
             </div>
-            <div className="genre-item">Adventure</div>
-            <div className="genre-item">Comedy</div>
-            <div className="genre-item">Drama</div>
-            <div className="genre-item">Horor</div>
-            <div className="genre-item">Romance</div>
-          </div>
           </div>
           <div className="main-recent">
             <div className="content-label"> Recent</div>
-            <div className="card-movie">
-                <img className="card-movie-foto" src={rednoticemovie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Red Notive</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2021</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
-                  </div>
-                </div>
-              </div>
-              <div className="card-movie">
-                <img className="card-movie-foto" src={armydeadmovie} />
-                <div className="card-content">
-                  <div className="card-content-left">
-                    <div className="card-content-left-title"> Army of The Dead</div>
-                    <div className="card-content-left-subtitle" style={{
-                      color: "#878787",
-                      fontSize: "12px",
-                    }}>2021</div>
-                  </div>
-                  <div className="card-content-right">
-                    <div className="card-content-action" style={{
-                      backgroundColor: "rgba(158,7,33,0.68)",
-                      borderRadius: "20px",
-                    }}>Watch Now</div>
-                    <div className="card-content-action" style={{
-                      backgroundColor: "#F6C600",
-                      color: "black",
-                      fontWeight: "bold",
-                    }}>ImDb 7.9</div>
-                  </div>
-                </div>
-              </div>
-            
+            {recent.map((item) => (
+               <div className="card-movie">
+               <img className="card-movie-foto" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}/>
+               <div className="card-content">
+                 <div className="card-content-left">
+                   <div className="card-content-left-title">{item.title}</div>
+                   <div
+                     className="card-content-left-subtitle"
+                     style={{
+                       color: "#878787",
+                       fontSize: "12px",
+                     }}
+                   >
+                     {item.release_date}
+                   </div>
+                 </div>
+                 <div className="card-content-right">
+                   <div
+                     className="card-content-action"
+                     style={{
+                       backgroundColor: "rgba(158,7,33,0.68)",
+                       borderRadius: "20px",
+                     }}
+                   >
+                     Watch Now
+                   </div>
+                   <div
+                     className="card-content-action"
+                     style={{
+                       backgroundColor: "#F6C600",
+                       color: "black",
+                       fontWeight: "bold",
+                     }}
+                   >
+                     ImDb {item.vote_average}
+                   </div>
+                 </div>
+               </div>
+             </div>
+            ))}
+           
           </div>
         </div>
       </div>
